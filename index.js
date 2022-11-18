fed9b3cdf37d7c202626da29d84389cf6148f77c
AD <AD@gmail.com>
2022-11-18 10:48:09 +0000

    basic layout drawn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+window.onload = () => {
+
+    let seconds = 0;
+    let tens = 0;
+
+    // Elements
+    let secondsElement = document.getElementById('seconds')
+    let tensElement = document.getElementById('tens')
+
+    // Buttons
+    let startButton = document.getElementById('start-button')
+    let stopButton = document.getElementById('stop-button')
+    let resetButton = document.getElementById('reset-button')
+
+    // Functionality
+    startButton.onclick = () => {
+        console.log('clicked');
+    }
+
+}
